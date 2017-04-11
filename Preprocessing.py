60239dad54dd0c55747c4789b8e664bfa386f559
AdityaDaflapurkar <adaflapurkar@gmail.com>
2017-04-11 23:30:59 +0530

    generate data function updated

--- Preprocessing.py
+++ Preprocessing.py
@@ -17,30 +17,73 @@ class DataProcessing:
 		return imgset
 
 
-	def read_data(self, datapath, n):
+	def generate_data(self, datapath, n):
 	# Read data from csv and convert it to a sequence of digit lists 
 		df=pd.read_csv(datapath)
 		nums=[]
 		print "Generating data... "
-		for i in range(0,n+1):
-			c=df.loc[df['FileName'] == str(i)+".png"]["DigitLabel"].tolist()
+		for i in range(1,n+1):
+			current_df = df.loc[df['FileName'] == str(i)+".png"]
+			x_list = current_df["Left"].tolist()
+			y_list = current_df["Top"].tolist()
+			dx = current_df["Width"].tolist()
+			dy = current_df["Height"].tolist()
+			
+			# Digits list
+			digits = current_df["DigitLabel"].tolist()
+			
+			# Top-left corner coordinates(x1,y1) 
+			x1 = min(x_list)
+			y1 = min(y_list)
+			
+			max_x = max(x_list)
+			max_y = max(y_list)
+			max_id_x = x_list.index(max_x)
+			max_id_y = y_list.index(max_y)
+
+			# Bottom-right corner coordinates()
+			x2 = max_x + dx[max_id_x]
+			y2 = max_y + dy[max_id_y]
+
 			# Alternative approach :
 			# img="'"+str(i)+'.png'+"'"		
 			# c=df.query("FileName == "+img)['DigitLabel']
+			
 			if i%100==0:
-				print i*100.0/(n+1)," percent done."
-			nums.append(c)
-		print "Successful."
+				progress = i*100.0/(n+1)
+				print "%.2f  percent done." % progress
+
+			current_record = self.get_record(digits, x1, y1, x2, y2)
+			nums.append(current_record)
+
+		print "100 percent done."
+		print "Data generation successful."
 		return nums
+
+
+	def get_record(self, digits, x1, y1, x2, y2):
+	# record in csv = [x1, y1, x2, y2, no_of_digits, digits, padding]	
+		record = []
+		
+		record.append(x1)
+		record.append(y1)
+		record.append(x2)
+		record.append(y2)
+		record.append(len(digits))
+		record = record + digits
+		padding = [-1]*(11-len(record))
+		record = record + padding
+		
+		return record
 
 
 	def write_to_csv(self, nums):
 	# Create target csv to get list of digits for each image record
 		datafile = open("svhn.csv", 'wb')
 		wr = csv.writer(datafile, quoting=csv.QUOTE_ALL)
-		dummy_header=[0,0,0,0,0,0]
+		dummy_header=["x1","y1","x2","y2","length","digit1","digit2","digit3","digit4","digit5","digit6"]
 		wr.writerow(dummy_header)
-		wr.writerows(nums[1:])
+		wr.writerows(nums)
 		
 
 	def k_fold_split(self, dataset, imageset,k):
@@ -52,6 +95,7 @@ class DataProcessing:
 		for i in xrange(k-1):
 			ilist.append(imageset[b:(l/k)+b])
 			dlist.append(dataset.ix[b:(l/k)+b-1])
+			#print dataset.ix[b:(l/k)+b-1]
 			b=(l/k)+b
 		ilist.append(imageset[b:])
 		dlist.append(dataset.ix[b:])
@@ -76,15 +120,14 @@ if __name__ == '__main__':
 	images=dp.get_images("svhn/data/train_images/*.png")
 	if(not(os.path.isfile("svhn.csv"))):
 		datapath = 'svhn/data/train.csv'
-		nums = dp.read_data(datapath,len(images))
+		nums = dp.generate_data(datapath,len(images))
 		dp.write_to_csv(nums)
 	
 	datapath = 'svhn.csv'
 	df=pd.read_csv(datapath)
 	
-	ilist, dlist = dp.k_fold_split(df,images,6)
+	ilist, dlist = dp.k_fold_split(df,images,5)
 	 
-	#for i in xrange(len(ilist)):
-	#	print len(ilist[i]),"img",i
-	#	print len(dlist[i]),"data",i
-	
+	for i in xrange(len(ilist)):
+		print len(ilist[i]),"image",i
+		print len(dlist[i]),"data",i
